dd5d7b6bc187018cc370330104905576add4af00
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-02-10 23:15:26 +0530

    Initial commit

---
+++ app/controllers/cart.js
@@ -0,0 +1,30 @@
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+
+export default class CartController extends Controller {
+  // @service shoppingCart;
+  @service('shopping-cart') cart;
+
+  get subtotal() {
+    return this.cart.itemList.reduce((acc, item) => {
+      return acc + item.price * item.count;
+    }, 0);
+  }
+  get tax() {
+    return 0.09 * this.subtotal;
+  }
+
+  get total() {
+    return this.subtotal + this.tax;
+  }
+  @action
+  updateItemCount(item, event) {
+    const count = event.target.value;
+    if (count >= 0) {
+      item.count = count;
+    } else {
+      item.count = 0;
+    }
+  }
+}
