3554254478c253169bb5162f86be0d8d1d976d4c
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-02-10 23:15:32 +0530

    Initial commit

---
+++ app/controllers/item.js
@@ -0,0 +1,23 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class ItemController extends Controller {
+  @tracked color = this.model.colors[0].color;
+
+  @tracked isZoomed = false;
+
+  get productImage() {
+    return this.model.colors.find(({ color }) => color === this.color)?.image;
+  }
+
+  @action
+  onChangeColor(newColor) {
+    this.color = newColor;
+  }
+
+  @action
+  toggleZoom() {
+    this.isZoomed = !this.isZoomed;
+  }
+}
