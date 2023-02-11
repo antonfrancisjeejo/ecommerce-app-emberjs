49672c76cbd11d9ccf8ef4dd1f814f2428926005
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-02-11 23:38:52 +0530

    Initial commit

---
+++ app/components/general-container.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+
+export default class GeneralContainerComponent extends Component {
+  @service shoppingCart;
+
+  get itemCount() {
+    return this.shoppingCart.itemList.reduce((total, item) => {
+      return (total += item.count);
+    }, 0);
+  }
+}
