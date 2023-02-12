6e5ed2004f73726bb30d53e22bdfee206d8df03d
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-02-12 22:28:38 +0530

    Initial commit

---
+++ app/routes/item.js
@@ -0,0 +1,18 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
+export default class ItemRoute extends Route {
+  @service store;
+
+  async model(params) {
+    const { item_id } = params;
+    const data = await this.store.findAll('product');
+    const product = data.find(({ id }) => id === item_id);
+    return product;
+  }
+
+  setupController(controller, model) {
+    super.setupController(controller, model);
+    controller.color = model.colors[0].color;
+  }
+}
